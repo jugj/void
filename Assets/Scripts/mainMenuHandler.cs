0248e66162db57a91e7ce8d392b1921e54edd6c8
Sören W. Oesterwind <jugj@users.noreply.github.com>
2022-08-06 19:07:21 +0200

    main menu vol slider

--- Assets/Scripts/mainMenuHandler.cs
+++ Assets/Scripts/mainMenuHandler.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class mainMenuHandler : MonoBehaviour
 {
     public string sceneToLoad = "Scenes/Credits";
+
+    public Slider volSlider;
 
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
@@ -28,6 +31,12 @@ public class mainMenuHandler : MonoBehaviour
 
     public void loadScene(string sceneToLoad) {
         SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+
+    public void SubmitSliderSetting()
+    {
+        //Displays the value of the slider in the console.
+        Debug.Log(volSlider.value);
     }
 
 }
