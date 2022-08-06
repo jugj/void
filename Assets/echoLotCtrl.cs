36c621ced61debce4515ccdf99ea896fda409d57
Sören W. Oesterwind <jugj@users.noreply.github.com>
2022-08-06 13:32:56 +0200

    working echolot (Sören)

--- Assets/echoLotCtrl.cs
+++ Assets/echoLotCtrl.cs
@@ -5,32 +5,45 @@ using UnityEngine;
 public class echoLotCtrl : MonoBehaviour
 {
 
-    public GameObject gameObjectBtnUp;
-    public GameObject gameObjectBtnDown;
-    public GameObject gameObjectLotIndicator;
+    public GameObject gameObjectLotIndicator, gameUpBtn, gameDownBtn, gameStopBtn;
 
-    private bool isUpPressed;
+    private bool isUpPressed, isDownPressed;
+
+    public float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
         isUpPressed = false;
+        isDownPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(isUpPressed) {
-            gameObjectLotIndicator.transform.Translate(Vector2.up * Time.deltaTime, Space.World);
-        
+            gameObjectLotIndicator.transform.Translate(Vector2.up * Time.deltaTime * speed, Space.World);
+        } else if(isDownPressed) {
+            gameObjectLotIndicator.transform.Translate(Vector2.down * Time.deltaTime * speed, Space.World);
         }
+        if(gameObjectLotIndicator.transform.position[1] >= 338) {
+            ButtonStop();
+        }
+        Debug.Log(gameObjectLotIndicator.transform.position);
     }
 
     public void ButtonUpPressed() {
         isUpPressed = true;
+        isDownPressed = false;
     }
 
-    public void ButtonUpUnPressed() {
+    public void ButtonDownPressed() {
+        isUpPressed = false;
+        isDownPressed = true;
+    }
+
+    public void ButtonStop() {
+        isDownPressed = false;
         isUpPressed = false;
     }
 }
