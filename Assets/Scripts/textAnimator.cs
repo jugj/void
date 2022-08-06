e228779a58a67e9b447e951763ca64016cf224a4
Sören W. Oesterwind <jugj@users.noreply.github.com>
2022-08-06 18:15:15 +0200

    messagescreen by Sören

---
+++ Assets/Scripts/textAnimator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class textAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject text1, text2;
+    public float delay = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
