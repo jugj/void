b22ba6f7075bf2f371a08c5f2fe6d3a9b024d29b
Sören W. Oesterwind <jugj@users.noreply.github.com>
2022-08-07 09:52:39 +0200

    Added death screen by Sören

---
+++ Assets/Scripts/DeathScreen/DeathScreenHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeathScreenHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject points1, holes, backHome;
+    void Start()
+    {
+        
+        points1.GetComponent<TextMeshProUGUI>().text = GameManagement.score.ToString();
+        holes.GetComponent<TextMeshProUGUI>().text = "A lot";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void handleBackButton() {
+        SceneManager.LoadScene("Scenes/MainMenu", LoadSceneMode.Single);
+    }
+}
